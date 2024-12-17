fb9e16ee462dd430b2e8e1e0c3150beac6272a79
harabat <45575009+harabat@users.noreply.github.com>
2024-12-17 15:03:13 +0100

    enable embedding

--- conf/superset_config.py
+++ conf/superset_config.py
@@ -26,6 +26,45 @@ ENABLE_PROXY_FIX = True
 
 DEFAULT_FEATURE_FLAGS = {
     "DASHBOARD_RBAC": True,
-    # "EMBEDDED_SUPERSET": True,
     "EMBEDDABLE_CHARTS": True,
+}
+
+ENABLE_CORS = True
+
+HTTP_HEADERS = {}
+
+TALISMAN_ENABLED = True
+
+TALISMAN_CONFIG = {
+    "content_security_policy": {
+        "base-uri": ["'self'"],
+        "default-src": ["'self'"],
+        "img-src": [
+            "'self'",
+            "blob:",
+            "data:",
+            "https://apachesuperset.gateway.scarf.sh",
+            "https://static.scarf.sh/",
+        ],
+        "worker-src": ["'self'", "blob:"],
+        "connect-src": [
+            "'self'",
+            "https://api.mapbox.com",
+            "https://events.mapbox.com",
+        ],
+        "object-src": "'none'",
+        "style-src": [
+            "'self'",
+            "'unsafe-inline'",
+        ],
+        "script-src": ["'self'", "'strict-dynamic'"],
+        "frame-ancestors": [
+            "*.dataactivists.org",
+            "localhost",
+            "127.0.0.1",
+        ],
+    },
+    "content_security_policy_nonce_in": ["script-src"],
+    "force_https": False,
+    "session_cookie_secure": False,
 }
